0d88db8eea43f69dd19c4c6acf74a8916b940820
Vinicius Gabriel de Lima da Silva <vinicius.silva@seventh.com.br>
2024-06-05 15:05:50 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Doação de Sangue</title>
+</head>
+<body>
+    
+    <?php
+
+        $idade == 60;
+        $peso == 49;
+
+        if($idade >= 16 && $idade <=69 && $peso <= 50) {
+            echo 'Atende aos requisitos';
+        }else {
+            echo 'Não atende aos requisitos';
+        }
+
+    ?>
+
+</body>
+</html>
